cad7c9df36b3625a4bca54235d7036b58b4a3890
Sebastian Castro <scastro@condorlabs.io>
2021-04-12 19:11:23 -0500

    Solved config

--- bin/files-config.sh
+++ bin/files-config.sh
@@ -1,55 +1,83 @@
 #!/usr/bin/env bash
-if [[ -z "$1" ]]; then
-  TYPE='"@scastro37/eslint-config/configJS"';
-elif [ $1 == 'js' ] || [ $1 == 'JS']; then
+read -n 1 -p "Install or Uninstall$*? [I/u] " ACTION
+echo 
+if test "$ACTION" = "i" -o "$ACTION" = "I"; then 
+  read -n 2 -p "JavaScript or TypeScript$*? [JS/ts] " TYPE
+  echo
+  if test "$TYPE" = "js" -o "$TYPE" = "JS"; then
     TYPE='"@scastro37/eslint-config/configJS"';
-elif [ $1 == "ts" ] || [ $1 == "TS" ]; then
-    TYPE='"@scastro37/eslint-config/configTS"'; fi
-
-if [ -f .eslintrc.js ];then rm .eslintrc.js;fi
-if [ -f .eslintrc.json ];then rm .eslintrc.json;fi
-if [ -f .eslintrc ];then rm .eslintrc;fi
-if [ -f .estint.config.js ];then rm .estint.config.js;fi
-if [ -f .estint.config.json ];then rm .estint.config.json;fi
-if [ -f .prettierrc.js ];then rm .prettierrc.js;fi
-if [ -f .prettierrc.json ];then rm .prettierrc.json;fi
-if [ -f .prettierrc ];then rm .prettierrc;fi
-if [ -f .prettier.config.js ];then rm .prettier.config.js;fi
-if [ -f .prettier.config.json ];then rm .prettier.config.json;fi
-npm i eslint@7.23.0 @scastro37/prettier-config @scastro37/eslint-config @scastro37/matchbox husky@6.0.0  -D
-ESLINT="module.exports = {extends: [${TYPE}]};"
-PRETTIER='"@scastro37/prettier-config"'
-SETTINGS='{
-  "[javascript]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode",
-    "editor.formatOnSave": true
-  },
-  "[typescript]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode",
-    "editor.formatOnSave": true
-  }
-}'
-printf "${PRETTIER} %s\n" > .prettierrc
-printf "${ESLINT} %s\n" > .eslintrc.js
-#Config setting.json
-node ./node_modules/@scastro37/prettier-config/mrm-config
-if [ -d .vscode ]; then 
-  if [ -f .vscode/settings.json ]; then
-    printf "${SETTINGS} %s\n" >> .vscode/settings.json; 
-  else printf "${SETTINGS} %s\n" > .vscode/settings.json; fi;
-else 
-  mkdir .vscode
-  printf "${SETTINGS} %s\n" > .vscode/settings.json;
-fi;
-
-RUTA=$( pwd )
-while [[ $COUNT -ne 6 ]]
-do
-  if [ -d .git ]; then
-    npx husky install
-    npx husky add .husky/pre-commit "cd $RUTA && npx lint-staged"
-    COUNT=$((5))
+  elif test "$TYPE" = "ts" -o "$TYPE" = "TS"; then
+    TYPE='"@scastro37/eslint-config/configTS"';
   else
-    cd ..; fi
-  COUNT=$(($COUNT+1))
-done;
+    TYPE='"@scastro37/eslint-config/configJS"'; fi
+  
+  if [ -f .eslintrc.js ];then rm .eslintrc.js;fi
+  if [ -f .eslintrc.json ];then rm .eslintrc.json;fi
+  if [ -f .eslintrc ];then rm .eslintrc;fi
+  if [ -f .estint.config.js ];then rm .estint.config.js;fi
+  if [ -f .estint.config.json ];then rm .estint.config.json;fi
+  if [ -f .prettierrc.js ];then rm .prettierrc.js;fi
+  if [ -f .prettierrc.json ];then rm .prettierrc.json;fi
+  if [ -f .prettierrc ];then rm .prettierrc;fi
+  if [ -f .prettier.config.js ];then rm .prettier.config.js;fi
+  if [ -f .prettier.config.json ];then rm .prettier.config.json;fi
+  npm i eslint@7.23.0 @scastro37/prettier-config @scastro37/eslint-config @scastro37/matchbox husky@6.0.0 -D --force
+  ESLINT="module.exports = {extends: [${TYPE}]};"
+  PRETTIER='"@scastro37/prettier-config"'
+  SETTINGS='{
+    "[javascript]": {
+      "editor.defaultFormatter": "esbenp.prettier-vscode",
+      "editor.formatOnSave": true
+    },
+    "[typescript]": {
+      "editor.defaultFormatter": "esbenp.prettier-vscode",
+      "editor.formatOnSave": true
+    }
+  }'
+  GITIGNORE="node_modules"
+
+  printf "${PRETTIER} %s\n" > .prettierrc
+  printf "${ESLINT} %s\n" > .eslintrc.js
+  #Config setting.json
+  if [ -d .vscode ]; then 
+    if [ ! -f .vscode/settings.json ]; then
+      touch .vscode/setting.json; fi 
+  else touch .vscode/setting.json; fi;
+
+  node ./node_modules/@scastro37/prettier-config/mrm-config
+
+  if [ -d node_modules ]; then
+    if [ ! -f .gitignore ]; then
+      printf "${GITIGNORE} %s\n" > .gitignore; fi
+  fi
+
+  RUTA=$( pwd )
+  while [[ $COUNT -ne 6 ]]
+  do
+    if [ -d .git ]; then
+      npx husky install
+      npx husky add .husky/pre-commit "cd $RUTA && npx lint-staged"
+      COUNT=$((5))
+    else
+      cd ..; fi
+    COUNT=$(($COUNT+1))
+  done;
+elif test "$ACTION" = "u" -o "$ACTION" = "U"; then
+  if [ -f .eslintrc.js ];then rm .eslintrc.js;fi  
+  if [ -f .eslintrc.json ];then rm .eslintrc.json;fi
+  if [ -f .eslintrc ];then rm .eslintrc;fi
+  if [ -f .estint.config.js ];then rm .estint.config.js;fi
+  if [ -f .estint.config.json ];then rm .estint.config.json;fi
+  if [ -f .prettierrc.js ];then rm .prettierrc.js;fi
+  if [ -f .prettierrc.json ];then rm .prettierrc.json;fi
+  if [ -f .prettierrc ];then rm .prettierrc;fi
+  if [ -f .prettier.config.js ];then rm .prettier.config.js;fi
+  if [ -f .prettier.config.json ];then rm .prettier.config.json;fi
+
+  node ./node_modules/@scastro37/prettier-config/mrm-uninstall
+  npm uninstall eslint @scastro37/prettier-config @scastro37/eslint-config @scastro37/matchbox husky -D
+  rm -rf .husky
+  rm -rf node_modules
+  rm package-lock.json
+  npm i;
+else echo "Exit..."; fi
